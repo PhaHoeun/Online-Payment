e0579a71991dd33866986fd975079ed628547973
pha_103ditech <hoeun.pha@103ditech.com>
2026-06-04 14:18:55 +0700

    update

--- lib/screen/paypal/paypal_screen.dart
+++ lib/screen/paypal/paypal_screen.dart
@@ -1,8 +1,6 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
-import 'package:online_payment/model/paypal_model/paypal_model.dart';
+import 'package:flutter_paypal_checkout/flutter_paypal_checkout.dart';
+
 
 class PaypalScreen extends StatefulWidget {
   const PaypalScreen({super.key});
@@ -12,90 +10,100 @@ class PaypalScreen extends StatefulWidget {
 }
 
 class _PaypalScreenState extends State<PaypalScreen> {
-  var paymentModel = PaypalModel(
-    amount: Amount(
-      total: '100',
-      currency: 'USD',
-      details: AmountDetails(
-        subtotal: '100',
-        shipping: '0',
-        shippingDiscount: 0,
-      ),
-    ),
-    description: 'The payment transaction description.',
-    itemList: ItemList(
-      items: [
-        Item(name: 'Apple', quantity: 4, price: '10', currency: 'USD'),
-        Item(name: 'Pineapple', quantity: 5, price: '12', currency: 'USD'),
-      ],
-    ),
-  );
-
-  // {
-  //                     "amount": {
-  //                       "total": '100',
-  //                       "currency": "USD",
-  //                       "details": {
-  //                         "subtotal": '100',
-  //                         "shipping": '0',
-  //                         "shipping_discount": 0,
-  //                       },
-  //                     },
-  //                     "description": "The payment transaction description.",
-  //                     "item_list": {
-  //                       "items": [
-  //                         {
-  //                           "name": "Apple",
-  //                           "quantity": 4,
-  //                           "price": '10',
-  //                           "currency": "USD",
-  //                         },
-  //                         {
-  //                           "name": "Pineapple",
-  //                           "quantity": 5,
-  //                           "price": '12',
-  //                           "currency": "USD",
-  //                         },
-  //                       ],
-  //                     },
-  //                   },
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Paypal Payment')),
+      appBar: AppBar(
+        title: const Text("PayPal Checkout", style: TextStyle(fontSize: 20)),
+      ),
       body: Center(
         child: TextButton(
-          onPressed: () {
+          onPressed: () async {
             Navigator.of(context).push(
               MaterialPageRoute(
-                builder: (BuildContext context) => PaypalCheckoutView(
+                builder: (BuildContext context) => PaypalCheckout(
                   sandboxMode: true,
-                  clientId:
-                      "AfOHBCBh8ugL6dxRhtE_264b6ueJA37TWt4iGnOUm9h0pftkaYCccw9h3y7ljg3RN9ntUehKQctaxHli",
-                  secretKey:
-                      "EN40zSNhSJEQT3F5WxCtH_6Iy-ACA3arlvl6gx_mcGTffRPDzLTt20nUEhi3H4dBXjV9LU1NmYdW13vR",
-                  transactions: [paymentModel.toJson()],
+                  clientId: "Ae81A58nfBL_AbI6vHJN3JthJswo5uifFzLPT0lp5LESGtphJKRD_lFiuOIpLlJj87MucNnhAVPNxw4W",
+                  secretKey: "EOzscYX2TtjCU2h2RaWvfeelpYz697zKCuDqc8vVCMkgpRUDNqLviMWAhJ_0dZwoKmMMc_pCYXTEjvnZ",
+                  returnURL: "success.snippetcoder.com",
+                  cancelURL: "cancel.snippetcoder.com",
+                  transactions: const [
+                    {
+                      "amount": {
+                        "total": '70',
+                        "currency": "USD",
+                        "details": {
+                          "subtotal": '70',
+                          "shipping": '0',
+                          "shipping_discount": 0,
+                        },
+                      },
+                      "description": "The payment transaction description.",
+                      // "payment_options": {
+                      //   "allowed_payment_method":
+                      //       "INSTANT_FUNDING_SOURCE"
+                      // },
+                      "item_list": {
+                        "items": [
+                          {
+                            "name": "Apple",
+                            "quantity": 4,
+                            "price": '5',
+                            "currency": "USD",
+                          },
+                          {
+                            "name": "Pineapple",
+                            "quantity": 5,
+                            "price": '10',
+                            "currency": "USD",
+                          },
+                        ],
+
+                        // shipping address is not required though
+                        //   "shipping_address": {
+                        //     "recipient_name": "Raman Singh",
+                        //     "line1": "Delhi",
+                        //     "line2": "",
+                        //     "city": "Delhi",
+                        //     "country_code": "IN",
+                        //     "postal_code": "11001",
+                        //     "phone": "+00000000",
+                        //     "state": "Texas"
+                        //  },
+                      },
+                    },
+                  ],
                   note: "Contact us for any questions on your order.",
                   onSuccess: (Map params) async {
-                    log("onSuccess: $params");
-                    Navigator.pop(context);
+                    print("onSuccess: $params");
                   },
                   onError: (error) {
-                    log("onError: $error");
+                    print("onError: $error");
                     Navigator.pop(context);
                   },
                   onCancel: () {
-                    debugPrint('cancelled:');
-                    Navigator.pop(context);
+                    print('cancelled:');
                   },
                 ),
               ),
             );
           },
-          child: const Text('Pay with paypal'),
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.teal,
+            foregroundColor: Colors.white,
+            shape: const BeveledRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(1)),
+            ),
+          ),
+          child: const Text('Checkout'),
         ),
       ),
     );
   }
 }
+
+
+// clientId:
+//                 'Ae81A58nfBL_AbI6vHJN3JthJswo5uifFzLPT0lp5LESGtphJKRD_lFiuOIpLlJj87MucNnhAVPNxw4W',
+//             secretKey:
+//                 'EOzscYX2TtjCU2h2RaWvfeelpYz697zKCuDqc8vVCMkgpRUDNqLviMWAhJ_0dZwoKmMMc_pCYXTEjvnZ',
